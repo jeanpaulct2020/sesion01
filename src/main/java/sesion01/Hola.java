05cdb3b1f98863885317927aa053fba39e76f62e
jean <jeancocht@gmail.com>
2020-10-03 00:53:56 -0500

    Mi primer commit

---
+++ src/main/java/sesion01/Hola.java
@@ -0,0 +1,21 @@
+package sesion01;
+
+import java.util.Scanner;
+
+public class Hola {
+	public static void main(String[] args) {
+		//imprimir mensaje
+		System.out.println("Bienvenido al curso");
+		
+		//capturar valores de la consola
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.println("¿cuál es tu nombre?: ");
+		
+		String nombre = sc.nextLine();
+		System.out.println("Bienvenido " + nombre);
+		
+		sc.close();
+		
+	}
+}
